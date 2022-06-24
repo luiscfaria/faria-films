af339779c30fde4421a38051a46e300403bb14e1
Luis Faria <luiscarlossfaria@gmail.com>
2022-06-24 11:48:38 -0300

    Started fetching data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React, {useEffect} from 'react'
 
+const App = () => {
+  
+  useEffect(() => {
+    const url = "https://api.themoviedb.org/3/trending/all/day?api_key=95b640c810615585379ac1368f019908"
 
-function App() {
+    const fetchData = async () => {
+      try {
+          const response = await fetch(url);
+          const json = await response.json();
+          console.log(json);
+      } catch (error) {
+          console.log("error", error);
+      }
+  };
+
+  fetchData()
+
+  })
+
   return (
-    <div className="App">
+    <div>
       <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
