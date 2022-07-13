71297de00f0d747c0d3455c42f1f27e508d8c50a
Luis <luiscarlossfaria@gmail.com>
2022-07-13 17:53:43 -0300

    Added anchor tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Header.styles.css'
+import "./Header.styles.css";
 import { Menu } from "antd";
 import { MailOutlined } from "@ant-design/icons";
 
@@ -8,14 +8,14 @@ const Header = () => {
     <div className="header">
       <div className="header-container">
         <Menu mode="horizontal" theme="dark" defaultSelectedKeys={["trending"]}>
-          <Menu.Item key="trending" icon={<MailOutlined />}>
-            Trending
+          <Menu.Item key="trending">
+            <a href="/">Trending</a>
           </Menu.Item>
-          <Menu.Item key="movies" icon={<MailOutlined />}>
-            Movies
+          <Menu.Item key="movies">
+            <a href="/movies">Movies</a>
           </Menu.Item>
-          <Menu.Item key="series" icon={<MailOutlined />}>
-            Series
+          <Menu.Item key="series">
+            <a href="/series">Series</a>
           </Menu.Item>
         </Menu>
       </div>
