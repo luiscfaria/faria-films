4c949cf06b810eeced632c41e7d4bb4245f53968
Luis Faria <luiscarlossfaria@gmail.com>
2022-07-21 18:08:38 -0300

    Small changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import TrendingPage from './pages/Trending Page/TrendingPage.jsx'
 import MoviesPage from './pages/Movies Page/MoviesPage.jsx'
 import SeriesPage from './pages/Series Page/SeriesPage.jsx'
 import ArtistsPage from './pages/Artist Page/ArtistPage.jsx'
-import DetailsPage from './pages/Details Page/DetailsPage.jsx';
-import './App.css';
+import DetailsPage from './pages/Details Page/DetailsPage.jsx'
+import './App.css'
 
-import Header from './components/Header.jsx';
+import Header from './components/Header.jsx'
 
 class App extends Component {
   render() {
     return (
-      <><Header />
-      <Routes>
-        <Route exact path="/" element={<TrendingPage />} />
-        <Route exact path="/movies" element={<MoviesPage />} />
-        <Route exact path="/series" element={<SeriesPage />} />
-        <Route exact path="/artists" element={<ArtistsPage />} />
-        <Route exact path="/details/:id" element={<DetailsPage />} />
-      </Routes></>
-    );
-  };
+      <>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<TrendingPage />} />
+          <Route exact path="/movies" element={<MoviesPage />} />
+          <Route exact path="/series" element={<SeriesPage />} />
+          <Route exact path="/artists" element={<ArtistsPage />} />
+          <Route exact path="/details/:id" element={<DetailsPage />} />
+        </Routes>
+      </>
+    )
+  }
 }
 
 export default App
--- src/components/ArtistPoster.jsx
+++ src/components/ArtistPoster.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 import './MoviePoster.styles.css'
 
 const ArtistPoster = ({ artist }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const image_path = 'https://image.tmdb.org/t/p/w500'
 
   return (
     <>
-      <div
-        className="poster"
-        // onClick={() => navigate(`/details/${movie.id}`)}
-        style={{
-          backgroundImage: `url(${image_path}${artist.profile_path})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-        }}
-      ></div>
+      <div 
+          style={{
+            marginTop: '0.8rem',
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'center'
+          }}>
+        <div
+          className="poster"
+          // onClick={() => navigate(`/details/${movie.id}`)}
+          style={{
+            backgroundImage: `url(${image_path}${artist.profile_path})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            marginBottom: '0'
+          }}
+        ></div>
+        <div style={{fontSize: '1.5rem', margin: '0'}}>{artist.name}</div>
+      </div>
     </>
   )
 }
